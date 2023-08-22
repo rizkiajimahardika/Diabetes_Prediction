95581e6358c061494618ed6c3d7a04cb8df53619
Rizki Aji Mahardika <79896604+rizkiajimahardika@users.noreply.github.com>
2023-08-22 15:57:13 +0700

    Update streamlit_diabetes_prediction.py

--- streamlit_diabetes_prediction.py
+++ streamlit_diabetes_prediction.py
@@ -13,34 +13,34 @@ st.title('Diabetes Prediction')
 col1, col2 = st.columns(2)
 
 with col1 :
-  Pregnancies = st.text_input('Enter the Pregnancies value')
+  Pregnancies = st.number_input('Enter the Pregnancies value')
 
 with col2 :
-  Glucose = st.text_input('Enter the Glucose value')
+  Glucose = st.number_input('Enter the Glucose value')
   
 with col1 :
-  BloodPressure = st.text_input('Enter the Blood Pressure value')
+  BloodPressure = st.number_input('Enter the Blood Pressure value')
 
 with col2 :
-  SkinThickness = st.text_input('Enter the Skin Thickness value')
+  SkinThickness = st.number_input('Enter the Skin Thickness value')
 
 with col1 :
-  Insulin = st.text_input('Enter the Insulin value')
+  Insulin = st.number_input('Enter the Insulin value')
 
 with col2 :
-  BMI = st.text_input('Enter the BMI value')
+  BMI = st.number_input('Enter the BMI value')
 
 with col1 :
-  DiabetesPedigreeFunction = st.text_input('Enter the Diabetes Pedigree Function value')
+  DiabetesPedigreeFunction = st.number_input('Enter the Diabetes Pedigree Function value')
 
 with col2 :
-  Age = st.text_input('Enter the Age value')
+  Age = st.number_input('Enter the Age value')
   
 # Prediction
 diabetes_diagnosis = ''
 
 if st.button('Diabetes Prediction Test'):
-  diabetes_prediction = model_diabetes.predict(float([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]]))
+  diabetes_prediction = model_diabetes.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
   
   if(diabetes_prediction[0]==1):
     diabetes_diagnosis = 'The patient has diabetes'
